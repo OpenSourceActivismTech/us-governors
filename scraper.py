d21b14c50d8c1748d7bf244b2741aba6b564f968
Josh Levinger <josh@levinger.net>
2016-06-13 21:52:08 -0700

    parse url

--- scraper.py
+++ scraper.py
@@ -9,16 +9,20 @@ elements = root.cssselect("table td p")
 governors = []
 for e in elements:
     lines = e.text_content().replace('\t', '').split('\n')
+
     gov = {}
     gov['state'] = lines[0]
-    gov['name'] = lines[1].replace('Office of ', '')
+    gov['name'] = lines[1].replace('Office of Governor ', '')
     gov['address_1'] = lines[2]
     gov['address_2'] = lines[3]
-    gov['city_state'] = lines[4]
+    gov['city_state_zip'] = lines[4]
     gov['phone'] = lines[5].replace('Phone: ', '').replace('/', '-')
     try:
         gov['fax'] = lines[6].replace('Fax: ', '').replace('/', '-')
-        gov['website'] = lines[7]
+    except IndexError:
+        pass
+    try:
+        gov['url'] = e.cssselect('a')[0].attrib['href']
     except IndexError:
         pass
 
