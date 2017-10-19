823cc4faddbcbce1c0f1ddaeaa948cdc8653d2e0
Josh Levinger <josh@levinger.net>
2017-10-19 14:59:20 -0700

    Normalize phone number output

--- scraper.py
+++ scraper.py
@@ -24,7 +24,7 @@ for gov_div in soup.select('.article-body .col-sm-6.col-md-4'):
         gov_data['fax'] = lines.pop(-1).replace('Fax: ', '').replace('/', '-')
 
     if 'Phone:' in lines[-1]:
-        gov_data['phone'] = lines.pop(-1).replace('Phone: ', '').replace('/', '-')
+        gov_data['phone'] = lines.pop(-1).replace('Phone: ', '').replace('/', '-').replace(' ', '')
 
     if re.search(r'\d{5}', lines[-1]):
         gov_data['city'], gov_data['state_abbr'], gov_data['zip'] = re.match(r'(?P<city>.*)\,\s(?P<state_abbr>[A-Z]{2})\s(?P<zip>[\d\-]+)$', lines.pop(-1)).groups()
